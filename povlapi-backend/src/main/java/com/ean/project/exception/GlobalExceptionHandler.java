547f470e59a1945364b43380f916a58feae12039
Povlean <1927079760@qq.com>
2023-08-27 21:50:11 +0800

    后端初始化完成

---
+++ povlapi-backend/src/main/java/com/ean/project/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.ean.project.exception;
+
+import com.ean.project.common.BaseResponse;
+import com.ean.project.common.ErrorCode;
+import com.ean.project.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author yupi
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage());
+    }
+}
---
+++ povlapi-backend/src/main/java/com/ean/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.ean.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ean.project.model.entity.User;
+
+/**
+ * @Entity com.yupi.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
